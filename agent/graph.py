4ab3f440ba0b3f359fca35fbadff5f72d67e0c21
Simran Karamchandani <simbkaramchandani@gmail.com>
2025-10-16 22:55:17 +0530

    added GraphState for data validation of state, moved output files of planning agent to the root directory

--- agent/graph.py
+++ agent/graph.py
@@ -9,9 +9,15 @@ import json
 from prompts import *
 from states import *
 
+import os
+groq_api_key = os.getenv("GROQ_API_KEY")
+
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-def planner_agent(state: dict)-> dict:
+OUTPUT_DIR = "output"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+def planner_agent(state: GraphState)-> dict:
     users_prompt = state["user_prompt"]
     response = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
     if response is None:
@@ -19,10 +25,10 @@ def planner_agent(state: dict)-> dict:
 
     # Save the output to plan_output.json
     try:
-        # Convert the structured object (Plan) to a dictionary and save as JSON
-        with open("plan_output.json", "w") as f:
+        file_path = os.path.join(OUTPUT_DIR, "plan_output.json")
+        with open(file_path, "w") as f:
             json.dump(response.dict(), f, indent=4)
-        print("Plan saved to plan_output.json")
+        print(f"Plan saved to {file_path}")
     except Exception as e:
         print(f"Error saving plan output: {e}")
 
@@ -31,7 +37,7 @@ def planner_agent(state: dict)-> dict:
 
 
 
-def architect_agent(state: dict)-> dict:
+def architect_agent(state: GraphState)-> dict:
     plan : Plan = state["plan"] #prev node(planner) output is plan which is input to this architect
     response = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
     if response is None:
@@ -40,16 +46,16 @@ def architect_agent(state: dict)-> dict:
 
     # Save the output to architect_output.json
     try:
-        # Convert the structured object (TaskPlan) to a dictionary and save as JSON
-        with open("architect_output.json", "w") as f:
+        file_path = os.path.join(OUTPUT_DIR, "architect_output.json")
+        with open(file_path, "w") as f:
             json.dump(response.dict(), f, indent=4)
-        print("TaskPlan saved to architect_output.json")
+        print(f"TaskPlan saved to {file_path}")
     except Exception as e:
         print(f"Error saving architect output: {e}")
 
     return {"task_plan": response}
 
-graph = StateGraph(dict)
+graph = StateGraph(GraphState)
 graph.add_node("planner", planner_agent)
 graph.add_node("architect", architect_agent)
 graph.add_edge("planner", "architect")
